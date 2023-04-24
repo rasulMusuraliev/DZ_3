32503599b5da33cc26b3bb6b6b7d604aaa0d67d3
rasulMusuraliev <127593630+rasulMusuraliev@users.noreply.github.com>
2023-04-24 22:35:16 +0600

    Add files via upload

---
+++ DZ6_Rasul_25-01/main.js
@@ -0,0 +1,20 @@
+// const words = document.getElementById('words')
+// const input = document.getElementById('int')
+//
+// const reverse = () => {
+//     let arr = []
+//     arr.push(input.value)
+//     arr.forEach(word => {
+//         const p = document.createElement('p')
+//         p.append(word.split('').reverse().join(''))
+//         words.append(p)
+//     })
+//     input.value = ''
+// }
+// input.onkeydown = e =>
+//     e.keyCode === 13 ?reverse() : false
+//
+
+
+
+
